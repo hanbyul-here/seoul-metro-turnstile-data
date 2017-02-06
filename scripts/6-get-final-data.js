58425657c5e7f15fe56353dd59423e66bc62ca9f
Hanbyul Jo <hanbyul@nyu.edu>
2017-02-05 23:18:19 -0500

    added selected station

--- scripts/6-get-final-data.js
+++ scripts/6-get-final-data.js
@@ -39,18 +39,17 @@ function readJson () {
 
   } else {
     writeFile(total, 'date-based');
-    var newtotal = makeStationBasedData(total);
-    writeFile(newtotal, 'station-based');
+    makeStationBasedData(total);
+
   }
 
 }
-
-
 
 function makeStationBasedData(data) {
   var newData = [];
   var totalData = {
     station_name: 'total',
+    dates: [],
     line_num: '',
     lat: '',
     lon: ''
@@ -62,38 +61,117 @@ function makeStationBasedData(data) {
         line_num: st.line_num,
         lat: st.lat,
         lon: st.lon,
+        transfer_line: [],
+        dates: [],
         station_cd: st.station_cd
     };
 
       for (var datum of data) {
         for (var station of datum.stations) {
           if(station.station_name === obj.station_name && station.line_num === obj.line_num) {
-            obj[datum.date] = [];
-            obj[datum.date].push({
-              entries: station.entries,
-              exits: station.exits
-            })
+            obj['dates'].push({
+              date: datum.date,
+              turnstile_data: [{
+                entries: station.entries,
+                exits: station.exits
+              }]
+            });
+          } else if(obj.station_name === station.station_name){
+            if (obj.transfer_line.indexOf(station.line_num) < 0) obj.transfer_line.push(station.line_num);
           }
         }
-        totalData[datum.date] = [];
-        totalData[datum.date].push({
-          entries: datum.totalEntries,
-          exits: datum.totalExits
-        });
+
+        totalData['dates'].push({
+          date: datum.date,
+          turnstile_data: [{
+            entries: datum.totalEntries,
+            exits: datum.totalExits
+          }]
+        })
       }
 
       newData.push(obj);
 
   }
 
-  newData.push(totalData)
-  return newData;
+  const originData = newData;
+
+  originData.push(totalData);
+  writeFile(originData, 'station-based');
+
+  originData.pop();
+  var mergedData = mergeStation(originData);
+  writeFile(mergedData, 'station-merged');
+
+  return originData;
 }
+
+function mergeStation (data) {
+
+  var mingledData = [];
+  var pushedData = [];
+  var originalData = data;
+
+  for(const currentStation of originalData) {
+    const newObj = {
+        station_name: currentStation.station_name,
+        line_num: currentStation.line_num,
+        lat: currentStation.lat,
+        lon: currentStation.lon,
+        dates: [],
+        transfer_line: currentStation.transfer_line,
+        station_cd: currentStation.station_cd
+    }
+    for(var i = 0; i<currentStation.dates.length; i++) {
+      var dateObj = Object.assign({}, currentStation.dates[i]);
+      newObj.dates.push(dateObj);
+    }
+    const sames = findSameStations(newObj, data);
+
+    for(const oneStation of sames) {
+      console.log('before    : '+newObj.dates[2].turnstile_data[0].exits);
+      for(let i = 0; i < newObj.dates.length; i++) {
+        const currentEntries = oneStation.dates[i].turnstile_data[0].entries;
+        const currentExits = oneStation.dates[i].turnstile_data[0].exits;
+
+        newObj.dates[i].turnstile_data[0].entries += currentEntries;
+        newObj.dates[i].turnstile_data[0].exits += currentExits;
+      }
+      console.log('after     : ' +newObj.dates[2].turnstile_data[0].exits)
+    }
+
+    if(pushedData.indexOf(newObj.station_name) < 0) {
+      mingledData.push(newObj);
+      pushedData.push(newObj.station_name);
+    }
+  }
+
+  return mingledData;
+
+}
+
+function findSameStations(obj, data) {
+  const sameStations = [];
+  for(const station of data) {
+    if(station.station_name === obj.station_name && station.line_num !== obj.line_num) {
+      sameStations.push(station);
+    }
+  }
+  return sameStations;
+}
+
+// function findObj(element, index, array) {
+//   for(const elem in array) {
+//     if(elem.station_name === element.station_name && elem.line_num !== element.line_num) {
+
+//     }
+//   }
+// }
 
 
 function writeFile(obj, fileName) {
   console.log('write ' + fileName +' file');
-  jsonfile.writeFileSync(__dirname + '/turnstile-data/'+contour+'min/'+ fileName + '.json', obj);
+  jsonfile.writeFileSync(__dirname + '/turnstile-data/'+contour+'min/trimmed-'+ fileName + dates[0]+'-'+dates[dates.length-1] + '.json', obj);
 }
 
 readJson();
--- scripts/json2geojson.js
+++ scripts/json2geojson.js
@@ -4,17 +4,40 @@ var jsonfile = require('jsonfile');
 
 var lineNum;
 
-fs.readFile(__dirname + '/station-data-with-location/lineSU.json', function(err, data) {
-  lineNum = JSON.parse(data).line;
-  var stationData = JSON.parse(data).stations;
+fs.readFile(__dirname + '/turnstile-data/30min/trimmed-station-merged20161029-20161217.json', function(err, data) {
+  fs.readFile(__dirname + '/turnstile-data/30min/trimmed-station-merged20151107-20151226.json', function(err, oldData) {
+    lineNum = JSON.parse(data).line;
+    console.log("hi!")
+    var oldStations = JSON.parse(oldData);
+    var stationData = JSON.parse(data);
 
-  var lineGeoJson = GeoJSON.parse(stationData, {Point: ['lat', 'lon']});
-  writeStationFile(lineGeoJson);
+    for(const station of stationData) {
+      for(const oldStation of oldStations) {
+        if (station.station_cd == oldStation.station_cd) {
+          station.previous_data = oldStation.dates;
+        }
+      }
+    }
+
+
+    // get all Key names
+    let keyNames = [];
+    const oneExample = stationData[0];
+    for (var keyName in oneExample) {
+      if ( keyName != 'lat' || keyName != 'lon') {
+        keyNames.push(keyName);
+      }
+    }
+
+    var lineGeoJson = GeoJSON.parse(stationData, {Point: ['lat', 'lon'], include: keyNames});
+
+    writeStationFile(lineGeoJson);
+  });
 });
 
 
 function writeStationFile(obj) {
   obj.line = lineNum;
   console.log("writing geojson")
-  jsonfile.writeFileSync(__dirname + '/station-data-with-location/line'+lineNum+'.geojson', obj);
+  jsonfile.writeFileSync(__dirname + '/../examples/assets/lets-see.geojson', obj);
 }
